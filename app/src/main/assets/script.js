30058287090f35bec7a504591b43b67baa56b841
Augusto Herbel <augustoherbel.it@gmail.com>
2019-10-28 10:52:04 -0300

    Added check for javascript interface in the webpage

--- app/src/main/assets/script.js
+++
@@ -1,4 +0,0 @@
-(function() {
-    window.webkit = { 'messageHandlers': { 'interOp': window.interOp } };
-    console.log('Matched webkit interface');
-})()
--- app/src/main/java/com/example/webviewjavascriptinterfaceexample/MainActivity.java
+++ app/src/main/java/com/example/webviewjavascriptinterfaceexample/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.webviewjavascriptinterfaceexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
 
-        setupWebViewClient();
         setupWebViewJavascriptInterface();
 
         webView.loadUrl("file:///android_asset/demo.html");
@@ -55,49 +48,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         webView.addJavascriptInterface(webViewJavascriptInterface, "interOp");
-    }
-
-    private void setupWebViewClient() {
-        webView.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onPageFinished(final WebView view, final String url) {
-                super.onPageFinished(view, url);
-                matchiOSJavascriptInterfaceRoute();
-            }
-        });
-    }
-
-    void matchiOSJavascriptInterfaceRoute() {
-        final byte[] fileBytes = getFileBytes("script.js");
-        injectScriptFile(fileBytes);
-    }
-
-    byte[] getFileBytes(final String fileName) {
-        final InputStream input;
-        final byte[] buffer;
-        try {
-            input = getAssets().open(fileName);
-            buffer = new byte[input.available()];
-            input.read(buffer);
-            input.close();
-        } catch (final IOException e) {
-            Log.e("TAG", e.getMessage(), e);
-            return new byte[0];
-        }
-        return buffer;
-    }
-
-    void injectScriptFile(final byte[] fileBytes) {
-        // String-ify the script byte-array using BASE64 encoding !!!
-        final String encoded = Base64.encodeToString(fileBytes, Base64.NO_WRAP);
-        webView.loadUrl("javascript:(function() {" +
-                "var parent = document.getElementsByTagName('head').item(0);" +
-                "var script = document.createElement('script');" +
-                "script.type = 'text/javascript';" +
-                // Tell the browser to BASE64-decode the string into your script !!!
-                "script.innerHTML = window.atob('" + encoded + "');" +
-                "parent.appendChild(script)" +
-                "})()");
     }
 }
